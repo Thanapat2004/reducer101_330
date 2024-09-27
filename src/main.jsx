729fa77919f47f000f3fd871de5753153be6c544
ธนพัทร บุญผัด <Sack.thanapat0157@gmail.com>
2024-09-28 02:06:17 +0700

    edit_ListComponentFile

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 import FormComponent from './FormComponent';
+import ListComponent from './ListComponent'; 
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <FormComponent></FormComponent>
+    <ListComponent></ListComponent>
   </StrictMode>,
-)
+);
